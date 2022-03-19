217691f89c09f1cc1ad174ae0c5cf57ffeab25a0
John Maximilian <2e0byo@gmail.com>
2022-03-19 21:15:07 +0000

    test: fix test for fadeable fades.

--- tests/test_fadeable.py
+++ tests/test_fadeable.py
@@ -11,24 +11,60 @@ def async_return(result):
     return f
 
 
-async def test_fade(mocker):
-    f = Fadeable()
-    f.duty = mocker.MagicMock()
-    f.duty.return_value = async_return(0)
-    await f.fade(50, 0.1)
-    f.duty.assert_has_calls([mocker.call(x) for x in range(51)])
-    f.duty.return_value = async_return(50)
-    await f.fade(0, 0.1)
-    f.duty.assert_has_calls([mocker.call(x) for x in range(50, -1, -1)])
+def concreter(abclass):
+    # https://stackoverflow.com/a/37574495/15452601
+    class concreteCls(abclass):
+        pass
+
+    concreteCls.__abstractmethods__ = frozenset()
+    return type("DummyConcrete" + abclass.__name__, (concreteCls,), {})
 
 
-async def test_percent_fade(mocker):
-    f = Fadeable()
-    f.duty = mocker.MagicMock()
+def mock_duty(x=None, val=[0]):
+    if x is not None:
+        val[0] = x
+    else:
+        return val[0]
+
+
+@pytest.mark.parametrize(
+    "start, end",
+    [
+        [50, 0],
+        [0, 50],
+        [10, 20],
+        [20, 10],
+    ],
+)
+async def test_fade(mocker, start, end):
+    f = concreter(Fadeable)
+    duty_mock = mocker.PropertyMock(return_value=start)
+    f.duty = duty_mock
     f.max_duty = 50
-    f.duty.return_value = async_return(0)
-    await f.fade_percent(1, 0.1)
-    f.duty.assert_has_calls([mocker.call(x) for x in range(51)])
-    f.duty.return_value = async_return(50)
-    await f.fade_percent(0, 0.1)
-    f.duty.assert_has_calls([mocker.call(x) for x in range(50, -1, -1)])
+    f.MAX_FADE_FREQ_HZ = 1_000_000  # prevent it from getting in the way
+    f = f()
+    await f.fade(duty=end, duration=0.01)
+    step = -1 if end < start else 1
+    duty_mock.assert_has_calls([mocker.call(x) for x in range(start, step, end + step)])
+
+
+@pytest.mark.parametrize(
+    "start, end",
+    [
+        [1, 0],
+        [0, 1],
+        [0.5, 0.7],
+        [0.7, 0.5],
+    ],
+)
+async def test_percent_fade(mocker, start, end):
+    f = concreter(Fadeable)
+    duty_mock = mocker.PropertyMock(return_value=round(50 * start))
+    f.duty = duty_mock
+    f.max_duty = 50
+    f.MAX_FADE_FREQ_HZ = 1_000_000  # prevent it from getting in the way
+    f = f()
+    await f.fade(percent_duty=end, duration=0.01)
+    step = -1 if end < start else 1
+    duty_mock.assert_called_with(round(end * 50))
+    assert duty_mock.call_count > 10
