252024571187654da04d7401f1163ee2ecc5367b
John Maximilian <2e0byo@gmail.com>
2022-05-21 17:40:09 +0100

    test: fix test for restart on load (lcd).

--- tests/test_lcd.py
+++ tests/test_lcd.py
@@ -1,41 +1,46 @@
-from rpi_clock.lcd import Lcd
 import pytest
+from rpi_clock.lcd import Lcd
+
 
 @pytest.fixture
 def lcd(tmp_path):
     l = Lcd(path=tmp_path / "lcd")
     return l
 
-def test_restart_on_init(lcd):
-    with lcd.path.open() as f:
-        assert f.read() == "\x1b[LI"
+
+def test_restart_on_init(tmp_path, mocker):
+    mocker.patch("rpi_clock.lcd.Lcd.restart")
+    lcd = Lcd(path=tmp_path / "lcd")
+    lcd.restart.assert_called_once()
+
 
 def test_write(lcd):
     lcd.write("hello")
     with lcd.path.open() as f:
         assert f.read() == "hello"
 
+
 def test_goto(lcd):
-    lcd.goto(1,1)
+    lcd.goto(1, 1)
     with lcd.path.open() as f:
-        assert f.read() == '\x1b[Lx001y001;'
+        assert f.read() == "\x1b[Lx001y001;"
+
 
 def test_1line(lcd):
     lcd[0] = "short line"
     with lcd.path.open() as f:
-        assert f.read() == 'short line      ' 
+        assert f.read() == "short line      "
     lcd[0] = "this is a long line longer than 16 chars"
     with lcd.path.open() as f:
         assert f.read() == "this is a long l"
+
 
 def test_2lines(lcd, mocker):
     lcd[0] = "short line"
     lcd.goto = mocker.MagicMock()
     with lcd.path.open() as f:
-        assert f.read() == 'short line      ' 
+        assert f.read() == "short line      "
     lcd[1] = "line 2"
     lcd.goto.assert_called_with(0, 1)
     with lcd.path.open() as f:
         assert f.read() == "line 2          "
-
-
