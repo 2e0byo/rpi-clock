1c91246dcbb9d43cb86a743a6d4643d7471dc21f
John Maximilian <2e0byo@gmail.com>
2022-06-12 18:05:21 +0100

    docs: docstrings.

--- rpi_clock/timer.py
+++ rpi_clock/timer.py
@@ -7,6 +7,7 @@ class Timer:
     """Asyncio timer, api based loosely on Peter Hinch's Delay_ms."""
 
     def __init__(self, duration_ms=1_000, fn: callable = None):
+        """Set up the timer."""
         self.task = None
         self.loop = asyncio.get_event_loop()
         self._duration = duration_ms / 1_000
@@ -14,6 +15,7 @@ class Timer:
 
     @property
     def duration(self):
+        """Get the current duration, in seconds."""
         return self._duration * 1_000
 
     @duration.setter
@@ -21,10 +23,12 @@ class Timer:
         self._duration = duration_ms / 1_000
 
     def trigger(self):
+        """Cancel and then launch the timer."""
         self.cancel()
         self.task = asyncio.create_task(self._trigger())
 
     def cancel(self):
+        """Cancel the timer."""
         try:
             self.task.cancel()
             self.task = None
@@ -33,6 +37,7 @@ class Timer:
 
     @property
     def running(self):
+        """Whether the timer is currently running."""
         return bool(self.task)
 
     async def _call(self):
