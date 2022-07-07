fa15741e690fdaa87e00ccc89b7cf739569e35a9
John Maximilian <2e0byo@gmail.com>
2022-07-07 18:45:33 +0100

    test: tests for lamp.

--- tests/test_fadeable.py
+++ tests/test_fadeable.py
@@ -1,7 +1,16 @@
 import asyncio
 
 import pytest
-from rpi_clock.fadeable import PWM, Fadeable, Lamp, MockFadeable
+from gpiozero.pins.mock import MockFactory
+from rpi_clock.fadeable import (
+    PWM,
+    Device,
+    Fadeable,
+    Lamp,
+    MockFadeable,
+    SpiControllerError,
+    SPIFixedRate,
+)
 
 
 async def test_set_duty():
@@ -117,3 +126,95 @@ async def test_pwm(mocker):
     f.pwm.change_duty_cycle.assert_called_once_with(56)
     f.pwm._duty_cycle = 56
     assert await f.get_duty() == 56
+
+
+@pytest.fixture
+def lamp(mocker):
+    Device.pin_factory = MockFactory()
+    f = Lamp(pin_factory=MockFactory())
+    f.spi.transfer = mocker.Mock()
+    yield f
+    f.spi.close()
+
+
+async def test_spi_rate_success(mocker):
+    Device.pin_factory = MockFactory()
+    spi = mocker.Mock()
+    rate = mocker.PropertyMock()
+    type(spi).rate = rate
+    pin_factory = MockFactory()
+    pin_factory.spi = mocker.Mock()
+    pin_factory.spi.return_value = spi
+
+    f = Lamp(pin_factory=pin_factory)
+    assert f.spi == spi
+    rate.assert_called_once()
+
+
+async def test_spi_rate_failure(mocker):
+    Device.pin_factory = MockFactory()
+    spi = mocker.Mock()
+    rate = mocker.PropertyMock()
+
+    def die(*_):
+        raise SPIFixedRate("test")
+
+    rate.side_effect = die
+    type(spi).rate = rate
+    pin_factory = MockFactory()
+    pin_factory.spi = mocker.Mock()
+    pin_factory.spi.return_value = spi
+
+    f = Lamp(pin_factory=pin_factory)
+    rate.assert_called_once()
+
+
+def _to_bytestr(x):
+    return b"s" + int(x).to_bytes(2, "little")
+
+
+async def test_lamp_set_get(lamp, mocker):
+    bytestr = _to_bytestr(150)
+    lamp.spi.transfer.return_value = bytestr[1:]
+    await lamp.set_duty(150)
+    lamp.spi.transfer.assert_any_call(bytestr)
+    assert await lamp.get_duty() == 150
+    assert await lamp.get_hardware_duty() == 150
+
+
+async def test_repeate_set(lamp, mocker):
+    bytestr = _to_bytestr(150)
+    calls = 0
+    lamp._logger = mocker.Mock()
+
+    def fail_once(*_):
+        nonlocal calls
+        calls += 1
+        if calls > 4:  # called twice per setting
+            return bytestr[1:]
+        else:
+            return _to_bytestr(0)
+
+    lamp.spi.transfer = fail_once
+    lamp._zero_task.cancel()
+    await lamp.set_duty(150)
+    assert calls == 6
+    lamp._logger.debug.assert_called_with("Set lamp to 150 after 2 attempts.")
+
+
+async def test_fail(lamp, mocker):
+    lamp.spi.transfer.return_value = b"\x00\x00"
+    lamp.reset = mocker.AsyncMock()
+    with pytest.raises(SpiControllerError):
+        await lamp.set_duty(150)
+    lamp.reset.assert_called()
+
+
+async def test_reset(lamp, mocker):
+    mocker.patch("rpi_clock.fadeable.asyncio.sleep", mocker.AsyncMock())
+    cs = mocker.Mock()
+    state = mocker.PropertyMock(return_value=1)
+    type(cs).state = state
+    lamp.cs = cs
+    await lamp.reset()
+    state.assert_has_calls([mocker.call(0), mocker.call(1)])
