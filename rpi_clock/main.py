2e0a3ddc8e9fa0cc98d3485e20e612bee5348c9d
John Morris <2e0byo@gmail.com>
2024-05-31 16:48:48 +0100

    chore: move main into clock.

--- rpi_clock/main.py
+++ rpi_clock/main.py
@@ -1,15 +1,16 @@
+from typing import Never
 import uvicorn
 
-from rpi_clock.api import app
-from rpi_clock.clock import loop
+from .api import app
+from .clock import loop
 
 
-async def start_server():
+async def start_server() -> Never:
     config = uvicorn.Config("main:app", host="0.0.0.0", port=8000)
     server = uvicorn.Server(config)
     await server.serve()
 
-
-if __name__ == "__main__":
+def main() -> None:
     loop.create_task(start_server())
     loop.run_forever()
+
