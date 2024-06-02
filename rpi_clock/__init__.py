1c3b9e51ce27db54d76a31c8e532a1908ffa721e
John Morris <2e0byo@gmail.com>
2024-06-02 23:00:22 +0100

    feat: import sync run.

--- rpi_clock/__init__.py
+++ rpi_clock/__init__.py
@@ -3,6 +3,8 @@ from typing import Callable, TypeVar
 
 T = TypeVar("T")
 
+# NOTE this api is terrible; just importing what was there.
+
 async def run(fn: Callable[[], T] | None) -> T | None:
     """Run a fn or coro."""
     if fn:
@@ -10,6 +12,16 @@ async def run(fn: Callable[[], T] | None) -> T | None:
         if asyncio.iscoroutine(x):
             x = await x
         return x
+
+
+def sync_run(fn: Callable[[], T] | None, *args, **kwargs) -> T | None:
+    """Run a fn or schedule a coro, nonblocking."""
+    if not fn:
+        return None
+    x = fn(*args, **kwargs)
     if asyncio.iscoroutine(x):
-        x = await x
-    return x
+        # Can't get the result
+        asyncio.create_task(x)
+        return None
+    else:
+        return x
