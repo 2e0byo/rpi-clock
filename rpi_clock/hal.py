77326a5399f42fd76190cecfb5cf2a78ae4d61e5
John Morris <2e0byo@gmail.com>
2024-06-08 16:19:16 +0100

    feat: create entrypoint to set up hardware.

--- rpi_clock/hal.py
+++ rpi_clock/hal.py
@@ -1,3 +1,6 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+
 from gpiozero import LED
 
 from . import pinmap
@@ -8,7 +11,6 @@ from .lcd import Lcd
 backlight = PWM(pinmap.BACKLIGHT_CHANNEL, name="backlight")
 lcd = Lcd(backlight=backlight)
 lamp = Lamp(name="lamp")
-
 
 volume = PWM(pinmap.VOLUME_CHANNEL, name="backlight")
 mute = LED(pinmap.MUTE_PIN, active_high=False)
@@ -22,3 +24,16 @@ down_button = ZeroButton(
 enter_button = ZeroButton(
     pinmap.ENTER_BUTTON_PIN, suppress=True, name="EnterButton", blocking=True
 )
+
+# TODO move this out: this is the main entrypoint
+
+
+@asynccontextmanager
+async def setup_hardware(_) -> AsyncIterator[None]:
+    mute.off()
+
+    await backlight.start()
+    await lamp.start()
+    await volume.start()
+
+    yield
